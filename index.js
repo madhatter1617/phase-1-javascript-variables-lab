e4e680c5dd9c7accbd662f044231395d267100ab
Rosa <rosarenee@att.net>
2021-06-28 11:35:03 -0500

    commit tree cbf0adb866ef554d40f93cb4546ee48580d67db7

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file!
-const companyName = "Scuber";
 
-let mostProfitableNeighborhood = "Chelsea";
+const companyName = 'Scuber'
 
-let companyCeo = "Susan Smith";
+let mostProfitableNeighborhood = 'Chelsea'
 
+let companyCeo = 'Susan Smith'
